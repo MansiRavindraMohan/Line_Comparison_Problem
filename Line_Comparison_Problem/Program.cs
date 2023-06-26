1d04e48c8259933b9020b79408d34cd526037294
MansiRavindraMohan <manasimohan29@gmail.com>
2023-06-26 12:44:48 +0530

    [Mansi]: Check length of two lines

---
+++ Line_Comparison_Problem/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Line_Comparison_Problem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            LineLength lineLength= new LineLength(2, 3, 4, 5);
+            lineLength.Calculate();
+            Console.ReadLine();
+        }
+    }
+}
